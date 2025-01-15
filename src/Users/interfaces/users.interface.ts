ae4d72f1c82db68d6832dbd7b3f50f6c19d02bee
sean-waev <98407121+sean-waev@users.noreply.github.com>
2025-01-15 11:01:39 -0800

    Add files via upload

---
+++ src/Users/interfaces/users.interface.ts
@@ -0,0 +1,16 @@
+export interface User {
+  id: string;
+  username: string;
+  privyId: string;
+  password?: string;
+  about?: string;
+  email?: string;
+  createdAt?: Date;
+  isFlagged?: number;
+  points?: number;
+  submissions?: string[];
+  comments?: string[];
+  upvotedSubmissions?: string[];
+  jobs?: string[];
+  replies?: string[];
+}
